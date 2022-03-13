2d8f10db722de50a02587d4d79b09d906cfa4cab
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-13 16:38:48 +0700

    add & run visualization

---
+++ visualization/main.py
@@ -0,0 +1,70 @@
+import logging
+import pandas as pd
+from visualization.utils import *
+
+log = logging.getLogger(__name__)
+
+
+def visualize_text(data, config, target_val, out_dir):
+    text_vars = config.variables.text_vars
+    bins = config.visualizations.hist_bins
+    for text_var in text_vars:
+        plot_word_cloud(
+            data[text_var],
+            get_save_path(
+                out_dir, "{}_{}_{}.png".format(target_val, text_var, "word_cloud")
+            ),
+        )
+        plot_word_count_distribution(
+            data[text_var],
+            get_save_path(
+                out_dir, "{}_{}_{}.jpg".format(target_val, text_var, "word_count_dist")
+            ),
+            bins,
+        )
+        plot_avg_word_len_distribution(
+            data[text_var],
+            get_save_path(
+                out_dir,
+                "{}_{}_{}.jpg".format(target_val, text_var, "avg_word_len_dist"),
+            ),
+            bins,
+        )
+        log.info("Visualization of {} text field successful".format(text_var))
+
+
+def visualize_categories(data, target_val, out_dir, config):
+    for cat_var in config.variables.cat_vars:
+        plot_category_distribution(
+            data[cat_var],
+            cat_var,
+            get_save_path(
+                out_dir, "{}_{}_{}.jpg".format(target_val, cat_var, "distribution")
+            ),
+        )
+        log.info("Visualization of {} distribution successful".format(cat_var))
+
+
+def visualize(in_path, target_val, out_dir, config):
+    target_var = config.variables.target_var
+
+    data = pd.read_csv(in_path).fillna("")
+    log.info("Read data from {} successful".format(in_path))
+
+    data[target_var] = target_val
+
+    visualize_text(data, config, target_val, out_dir)
+    log.info("Done text fields visualization of {}".format(in_path))
+
+    visualize_categories(data, target_val, out_dir, config)
+    log.info("Done categorical fields visualization of {}".format(in_path))
+
+    return data
+
+
+def visualize_label_distribution(data, out_dir, config):
+    plot_label_distribution(
+        data, get_save_path(out_dir, "label_distribution.jpg"), config
+    )
+
+    log.info("Visualization of label distribution successful")
---
+++ visualization/utils.py
@@ -0,0 +1,71 @@
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from wordcloud import WordCloud, STOPWORDS
+
+
+def get_save_path(out_folder, *paths):
+    return os.path.join(out_folder, *paths)
+
+
+def word_counter(news):
+    return len(news.split())
+
+
+def get_avg_word_len(news):
+    return np.average([len(word) for word in news.split()])
+
+
+def plot_label_distribution(data, save_path, config):
+    target_var = config.variables.target_var
+    label_dist = data[target_var].value_counts(normalize=True).reset_index()
+
+    ax = label_dist.plot.bar(
+        y=target_var, x="index", color=config.style.target_colors, width=0.4
+    )
+    ax.bar_label(ax.containers[0], fmt="%.2f")
+    ax.get_legend().remove()
+    plt.xlabel("Is fake news ?")
+    plt.ylabel("Proportion")
+    plt.title("Label distribution of news")
+    plt.xticks(rotation=0)
+    plt.savefig(save_path, dpi=200, bbox_inches="tight")
+    plt.close()
+
+
+def plot_word_cloud(texts, save_path):
+    word_cloud = WordCloud(stopwords=STOPWORDS, width=1600, height=800).generate(
+        " ".join(texts)
+    )
+    word_cloud.to_file(save_path)
+
+
+def plot_word_count_distribution(texts, save_path, bins="auto"):
+    plt.figure(figsize=(8, 5))
+    sns.histplot(texts.apply(word_counter), stat="density", bins=bins)
+    plt.xlabel("Word count interval")
+    plt.savefig(save_path, dpi=200, bbox_inches="tight")
+    plt.close()
+
+
+def plot_avg_word_len_distribution(texts, save_path, bins="auto"):
+    plt.figure(figsize=(8, 5))
+    sns.histplot(texts.apply(get_avg_word_len), stat="density", bins=bins)
+    plt.xlabel("Average word length interval")
+    plt.savefig(save_path, dpi=200, bbox_inches="tight")
+    plt.close()
+
+
+def plot_category_distribution(category, column_name, save_path):
+    var_name_cap = str(column_name).capitalize()
+    cat_dist = category.value_counts(normalize=True).reset_index()
+    plt.figure(figsize=(8, 4))
+    ax = sns.barplot(y=cat_dist.columns[-1], x="index", data=cat_dist)
+    ax.bar_label(ax.containers[0], fmt="%.2f")
+    plt.xlabel(var_name_cap)
+    plt.ylabel("Proportion")
+    plt.title("{} distribution of news".format(var_name_cap))
+    plt.xticks(rotation=0)
+    plt.savefig(save_path, dpi=200, bbox_inches="tight")
+    plt.close()
