16619c2704efc117737bde23029a0d104f288a93
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-14 16:26:59 +0700

    refactor utils

--- modeling/utils.py
+++ modeling/utils.py
@@ -1,7 +1,15 @@
+import os
+from pyspark.sql import SparkSession
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import CountVectorizer, VectorAssembler
 from pyspark.sql.functions import split
+from hydra.utils import get_original_cwd
+from pyspark.sql.functions import lit
+
+
+def create_spark_session(app_name):
+    return SparkSession.builder.appName(app_name).getOrCreate()
 
 
 def combine_data(data):
@@ -13,7 +21,6 @@ def combine_data(data):
 
 
 def explode_text(dataset, text_features):
-
     acc = dataset
     result_column = []
 
@@ -25,8 +32,37 @@ def explode_text(dataset, text_features):
     return acc, result_column
 
 
-def get_count_vectorizer(text_features):
+def read_and_transform_datasets(session, config):
+    dfs = []
+    target_var = config.variables.target_var
+    current_path = get_original_cwd()
 
+    for idx, in_path in enumerate(config.dataset.input_paths):
+        full_in_path = os.path.join(current_path, in_path)
+        header = config.dataset.headers[idx]
+        target_val = config.variables.target_vals[idx]
+        df = session.read.csv(full_in_path, header=header, inferSchema=True)
+        df = df.withColumn(target_var, lit(target_val)).fillna("")
+        dfs.append(df)
+
+    return dfs
+
+
+def get_train_test_set(session, config):
+    dfs = read_and_transform_datasets(session, config)
+    combined = combine_data(dfs)
+
+    data, text_col_name = explode_text(combined, config.variables.text_vars)
+
+    train_split = float(config.splitting.train_split)
+    train_set, test_set = data.randomSplit(
+        [train_split, 1 - train_split], seed=config.splitting.seed
+    )
+
+    return train_set, test_set, text_col_name
+
+
+def get_count_vectorizer(text_features):
     vectorizers = []
     result_column = []
 
@@ -40,7 +76,6 @@ def get_count_vectorizer(text_features):
 
 
 def get_pipeline(text_col_name, model, features):
-
     count_vectorizers, vector_names = get_count_vectorizer(text_col_name)
     assembler = VectorAssembler(inputCols=vector_names, outputCol=features)
 
