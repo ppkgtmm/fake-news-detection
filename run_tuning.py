853651e6bc85d68cfee55e7fa787f277524b0bed
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-14 21:54:19 +0700

    add tuning runner

---
+++ run_tuning.py
@@ -0,0 +1,24 @@
+import hydra
+import logging
+from modeling import do_tuning
+
+log = logging.getLogger(__name__)
+
+
+@hydra.main(config_path="config", config_name="modeling.yaml")
+def run_tuning(config):
+
+    assert len(config.dataset.input_paths) == len(config.dataset.headers)
+    assert len(config.dataset.headers) == len(config.dataset.target_vals)
+
+    log.info("Config param validation successful")
+
+    log.info("Begin parameter tuning process")
+
+    do_tuning(config)
+
+    log.info("End parameter tuning process")
+
+
+if __name__ == "__main__":
+    run_tuning()
