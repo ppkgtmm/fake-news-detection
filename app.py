d894e64d86cb6547d6361273b49d3308dceb1797
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-17 00:52:42 +0700

    add info endpoint

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ import json
 from fastapi import FastAPI
 from schemas import PredictionInput, Predictions
 from inference import do_prediction
+from hydra import compose, initialize
 
 app = FastAPI()
+
+
+@app.get("/info")
+def get_app_info():
+    with initialize(config_path="config", job_name="inference_app"):
+        config = compose(config_name="app.yaml")
+    return {
+        "app_name": config.app.name,
+        "version": config.app.version,
+    }
 
 
 @app.post("/predict")
