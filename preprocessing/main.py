0ef4a275abfad445f70562c4654d9894c8b206a8
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-19 14:20:18 +0700

    update prep runner

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -8,6 +8,9 @@ log = logging.getLogger(__name__)
 def preprocess(in_path, out_path, config):
     data = pd.read_csv(in_path)
 
+    if config.variables.drop_vars:
+        data.drop(columns=config.variables.drop_vars, inplace=True)
+
     data.loc[:, config.variables.text_vars].fillna("", inplace=True)
 
     for text_var in config.variables.text_vars:
